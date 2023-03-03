d91b5fc221ebb9149b49b8d1ea154cecc9b71a97
Antsently <stupidlymymail@gmail.com>
2023-03-03 17:59:34 +0200

    Размещение проекта на Гите!

--- Собери компьютер САМ/main.Designer.vb
+++ Собери компьютер САМ/main.Designer.vb
@@ -22,8 +22,16 @@ Partial Class main
     'Не изменяйте ее в редакторе исходного кода.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(642, 396)
+        Me.Name = "main"
         Me.Text = "main"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
